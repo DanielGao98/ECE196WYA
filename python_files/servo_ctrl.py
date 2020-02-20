84fc70a07acd3a7cbb038704fb268ce3c50b1042
Danny Gao <dannygao0818@gmail.com>
2020-02-20 11:06:10 -0800

    Servo Demo

--- python_files/servo_ctrl.py
+++ python_files/servo_ctrl.py
@@ -12,11 +12,22 @@ servoPIN = 11  # change
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-servo = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
+servo = GPIO.PWM(servoPIN, 50)  # GPIO 11 for PWM with 50Hz
 servo.start(0)  # Initialization
 time.sleep(2)
 
 servo.ChangeDutyCycle(5)
 time.sleep(0.5)  # wait to reach
+servo.ChangeDutyCycle(7)
+time.sleep(0.5)
+servo.ChangeDutyCycle(12)
+time.sleep(0.5)
+servo.ChangeDutyCycle(2)
+time.sleep(0.5)
+
 servo.ChangeDutyCycle(0)  # no pulse
 time.sleep(0.7)
+
+
+servo.stop()
+GPIO.cleanup()
