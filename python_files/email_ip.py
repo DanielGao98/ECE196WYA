84372645d982f1aac596cc3d60b60c3b66b9ce5d
kevinyoussef <kryousse@ucsd.edu>
2020-02-22 17:27:05 -0800

    Editted email_ip.py

--- python_files/email_ip.py
+++ python_files/email_ip.py
@@ -1,15 +1,13 @@
 import smtplib
 import os
-print('Enter Reciever\'s email')
-reciever = input()
 
 def get_ip():
     return os.popen('hostname -I').read().split()[0]
 
-def send(reciever = reciever):
+def send():
     sender = 'wya196wya@gmail.com'
     password = 'wya196wya'
-
+    reciever = 'wya196wya@gmail.com'
     #Send the mail
     msg = "\n"+get_ip()  # The /n separates the message from the headers
 
