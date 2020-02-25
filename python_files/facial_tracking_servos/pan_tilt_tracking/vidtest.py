e54b717160695a57f32632132b28e3c3a96ed3fa
sonyamohammed <44535724+sonyamohammed@users.noreply.github.com>
2020-02-25 11:57:33 -0800

    Update vidtest.py

--- python_files/facial_tracking_servos/pan_tilt_tracking/vidtest.py
+++ python_files/facial_tracking_servos/pan_tilt_tracking/vidtest.py
@@ -2,7 +2,7 @@
 from .webcamvideostream import WebcamVideoStream
 
 class VideoStream:
-	def __init__(self, src=0, usePiCamera=False, resolution=(320, 240),
+	def __init__(self, src=0, usePiCamera=True, resolution=(320, 240),
 		framerate=32, **kwargs):
 		# check to see if the picamera module should be used
 		if usePiCamera:
