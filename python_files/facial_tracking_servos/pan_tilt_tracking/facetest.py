118a77aed36139c8e50864cc9963f764fe46c2f8
sonya <sonyamohammed99@gmail.com>
2020-02-29 15:42:21 -0800

    facetest

---
+++ python_files/facial_tracking_servos/pan_tilt_tracking/facetest.py
@@ -0,0 +1,120 @@
+# python facetest.py --cascade haarcascade_frontalface_default.xml
+
+# import necessary packages
+from multiprocessing import Manager
+from multiprocessing import Process
+from imutils.video import VideoStream
+from pyimagesearch.objcenter import ObjCenter
+from pyimagesearch.pid import PID
+import argparse
+import pantilthat
+import signal
+import time
+import sys
+import cv2
+
+# function to handle keyboard interrupt
+def signal_handler(sig, frame):
+	print("[INFO] You pressed `ctrl + c`! Exiting...")
+	sys.exit()
+
+def obj_center(args, objX, objY, centerX, centerY):
+	# signal trap to handle keyboard interrupt
+	signal.signal(signal.SIGINT, signal_handler)
+
+	# start the video stream and wait for the camera to warm up
+	vs = VideoStream(usePiCamera=True).start()
+	time.sleep(2.0)
+
+	# initialize the object center finder
+	obj = ObjCenter(args["cascade"])
+
+	# loop indefinitely
+	while True:
+		# grab the frame from the threaded video stream and flip it
+		# vertically (since our camera was upside down)
+		frame = vs.read()
+	#	frame = cv2.flip(frame, 0)
+
+		# calculate the center of the frame as this is where we will
+		# try to keep the object
+		(H, W) = frame.shape[:2]
+		centerX.value = W // 2
+		centerY.value = H // 2
+
+		# find the object's location
+		objectLoc = obj.update(frame, (centerX.value, centerY.value))
+		((objX.value, objY.value), rect) = objectLoc
+
+		# extract the bounding box and draw it
+		if rect is not None:
+			(x, y, w, h) = rect
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0),
+				2)
+
+		# display the frame to the screen
+		cv2.imshow("Pan-Tilt Face Tracking", frame)
+		cv2.waitKey(1)
+
+def pid_process(output, p, i, d, objCoord, centerCoord):
+	# signal trap to handle keyboard interrupt
+	signal.signal(signal.SIGINT, signal_handler)
+
+	# create a PID and initialize it
+	p = PID(p.value, i.value, d.value)
+	p.initialize()
+
+	# loop indefinitely
+	while True:
+		# calculate the error
+		error = centerCoord.value - objCoord.value
+
+		# update the value
+		output.value = p.update(error)
+
+def in_range(val, start, end):
+	# determine the input vale is in the supplied range
+	return (val >= start and val <= end)
+
+# check to see if this is the main body of execution
+if __name__ == "__main__":
+	# construct the argument parser and parse the arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-c", "--cascade", type=str, required=True,
+		help="path to input Haar cascade for face detection")
+	args = vars(ap.parse_args())
+
+	# start a manager for managing process-safe variables
+	with Manager() as manager:
+		# set integer values for the object center (x, y)-coordinates
+		centerX = manager.Value("i", 0)
+		centerY = manager.Value("i", 0)
+
+		# set integer values for the object's (x, y)-coordinates
+		objX = manager.Value("i", 0)
+		objY = manager.Value("i", 0)
+
+		# set PID values for panning
+		panP = manager.Value("f", 0.09)
+		panI = manager.Value("f", 0.08)
+		panD = manager.Value("f", 0.002)
+
+		# set PID values for tilting
+		tiltP = manager.Value("f", 0.11)
+		tiltI = manager.Value("f", 0.10)
+		tiltD = manager.Value("f", 0.002)
+
+		# we have 4 independent processes
+		# 1. objectCenter  - finds/localizes the object
+		# 2. panning       - PID control loop determines panning angle
+		# 3. tilting       - PID control loop determines tilting angle
+		# 4. setServos     - drives the servos to proper angles based
+		#                    on PID feedback to keep object in center
+		processObjectCenter = Process(target=obj_center,
+			args=(args, objX, objY, centerX, centerY))
+	
+		# start all 4 processes
+		processObjectCenter.start()
+            
+		# join all 4 processes
+		processObjectCenter.join()
