4262e36b1200f9aa0dbc14d22f84502f40307680
sonya <sonyamohammed99@gmail.com>
2020-03-08 14:36:43 -0700

    Changed naming

---
+++ python_files/facial_tracking_servos/pan_tilt_tracking/cv_tester.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+        a, frame = cap.read()
+        cv2.imshow("Frame", frame)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+cap.release()
+cv2.destroyAllWindows()
