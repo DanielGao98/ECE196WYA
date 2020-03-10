51623c5fe6a4cbf41890119b1ef50eaecfce0fac
kevinyoussef <kryousse@ucsd.edu>
2020-03-10 11:31:56 -0700

    Editted read_data.py

--- python_files/Kevin_Attempt/read_data.py
+++ python_files/Kevin_Attempt/read_data.py
@@ -1,5 +1,7 @@
 import pyrebase
 import cv2
+import numpy as np
+import time
 config = {
   "apiKey": "AIzaSyDQUF3pJGSI_CEflk2n8OV1DOa3m_R72uE",
   "authDomain": "ece196wya.firebaseapp.com",
@@ -13,7 +15,23 @@ db = firebase.database()
 
 
 while True:
-    arr = db.child("arr").get().val()
-    print("arr = ", arr)
-    cv2.imshow('arr',arr)
+  time.sleep(0.1)
+  frame = np.asarray(db.child("frame").get().val())
+  print(frame)
+  print(frame.shape)
+  cv2.imshow('frame',frame)
+  cv2.waitKey(0)
+  '''
+  try:
+    cv2.imshow('frame',frame)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == '27':
+      break
+    print("frame = ", frame)
+  except:
+    print("Empty arrays")
+
+cv2.destroyAllWindows()
+'''
 
